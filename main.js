38ecaddc450aaeb7f6c54fd6272ec56f20f5503c
upm1 <72426046+upm1@users.noreply.github.com>
2021-12-07 00:30:08 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,191 @@
+import prodb, {
+  bulkcreate,
+  createEle,
+  getData,
+  SortObj
+} from "./module.js";
+
+
+let db = prodb("Productdb", {
+  products: `++id, nombre, apellidos, edad, hora,fecha,nac`
+});
+
+// input tags
+const userid = document.getElementById("userid");
+const nombre = document.getElementById("nombre");
+const apellidos = document.getElementById("apellidos");
+const edad  = document.getElementById("edad");
+const hora = document.getElementById("hora");
+const fecha = document.getElementById("fecha");
+const nac = document.getElementById("nac");
+
+// create button
+const btncreate = document.getElementById("btn-create");
+const btnread = document.getElementById("btn-read");
+const btnupdate = document.getElementById("btn-update");
+const btndelete = document.getElementById("btn-delete");
+
+// user data
+
+// event listerner for create button
+btncreate.onclick = event => {
+  // insert values
+  let flag = bulkcreate(db.products, {
+    nombre: nombre.value,
+    apellidos: apellidos.value,
+    edad: edad.value,
+    hora: hora.value,
+    fecha: fecha.value,
+    nac: nac.value
+  });
+  
+  nombre.value = apellidos.value = edad.value = hora.value = fecha.value = nac.value = "";
+
+  // establecer el valor del cuadro de texto de identificación
+  getData(db.products, data => {
+    userid.value = data.id + 1 || 1;
+  });
+  table();
+
+  let insertmsg = document.querySelector(".insertmsg");
+  getMsg(flag, insertmsg);
+};
+
+// event listerner for create button
+btnread.onclick = table;
+
+// button update
+btnupdate.onclick = () => {
+  const id = parseInt(userid.value || 0);
+  if (id) {
+    // call dexie update method
+    db.products.update(id, {
+      nombre: nombre.value,
+      apellidos: apellidos.value,
+      edad: edad.value,
+      hora: hora.value,
+      fecha: fecha.value,
+      nac: nac.value
+    }).then((updated) => {
+      // let get = updated ? `data updated` : `couldn't update data`;
+      let get = updated ? true : false;
+
+      // display message
+      let updatemsg = document.querySelector(".updatemsg");
+      getMsg(get, updatemsg);
+
+      nombre.value = apellidos.value = edad.value = hora.value = fecha.value = nac.value = "";
+      //console.log(get);
+    })
+  } else {
+    console.log(`Please Select id: ${id}`);
+  }
+}
+
+// delete button
+btndelete.onclick = () => {
+  db.delete();
+  db = prodb("Productdb", {
+    products: `++id, nombre, apellidos, edad,hora,fecha,nac`
+  });
+  db.open();
+  table();
+  textID(userid);
+  // display message
+  let deletemsg = document.querySelector(".deletemsg");
+  getMsg(true, deletemsg);
+}
+
+window.onload = event => {
+  // set id textbox value
+  textID(userid);
+};
+
+
+
+
+// create dynamic table
+function table() {
+  const tbody = document.getElementById("tbody");
+  const notfound = document.getElementById("notfound");
+  notfound.textContent = "";
+  // remove all childs from the dom first
+  while (tbody.hasChildNodes()) {
+    tbody.removeChild(tbody.firstChild);
+  }
+
+
+  getData(db.products, (data, index) => {
+    if (data) {
+      createEle("tr", tbody, tr => {
+        for (const value in data) {
+          createEle("td", tr, td => {
+            td.textContent = data.edad === data[value] ? ` ${data[value]} año(s) ` : data[value];
+          });
+        }
+        createEle("td", tr, td => {
+          createEle("i", td, i => {
+            i.className += "fas fa-edit btnedit";
+            i.setAttribute(`data-id`, data.id);
+            // store number of edit buttons
+            i.onclick = editbtn;
+          });
+        })
+        createEle("td", tr, td => {
+          createEle("i", td, i => {
+            i.className += "fas fa-trash-alt btndelete";
+            i.setAttribute(`data-id`, data.id);
+            // store number of edit buttons
+            i.onclick = deletebtn;
+          });
+        })
+      });
+    } else {
+      notfound.textContent = "BD esta vacia...!";
+    }
+
+  });
+}
+
+const editbtn = (event) => {
+  let id = parseInt(event.target.dataset.id);
+  db.products.get(id, function (data) {
+    let newdata = SortObj(data);
+    userid.value = newdata.id || 0;
+    nombre.value = newdata.nombre || "";
+    apellidos.value = newdata.apellidos || "";
+    edad.value = newdata.edad || "";
+    hora.value = newdata.hora || "";
+    fecha.value = newdata.fecha || "";
+    nac.value = newdata.nac || "";
+
+  });
+}
+
+// eliminar icono eliminar elemento
+const deletebtn = event => {
+  let id = parseInt(event.target.dataset.id);
+  db.products.delete(id);
+  table();
+}
+
+// textbox id
+function textID(textboxid) {
+  getData(db.products, data => {
+    textboxid.value = data.id + 1 || 1;
+  });
+}
+
+// function msg
+function getMsg(flag, element) {
+  if (flag) {
+    // call msg 
+    element.className += " movedown";
+
+    setTimeout(() => {
+      element.classList.forEach(classname => {
+        classname == "movedown" ? undefined : element.classList.remove('movedown');
+      })
+    }, 4000);
+  }
+}
---
+++ sw.js
@@ -0,0 +1,61 @@
+;
+//asignar un nombre y versión al cache
+const CACHE_NAME = 'v1_Registros',
+  urlsToCache = [
+    './',
+    'https://fonts.googleapis.com/css?family=Raleway:400,700',
+    'https://fonts.gstatic.com/s/raleway/v12/1Ptrg8zYS_SKggPNwJYtWqZPAA.woff2',
+    'https://use.fontawesome.com/releases/v5.0.7/css/all.css',
+    'https://use.fontawesome.com/releases/v5.0.6/webfonts/fa-brands-400.woff2',
+    './style.css',
+    './script.js',
+    './img/icon-512x512.png',
+    './img/icon-512x512.png'
+  ]
+
+//durante la fase de instalación, generalmente se almacena en caché los activos estáticos
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => self.skipWaiting())
+      })
+      .catch(err => console.log('Falló registro de cache', err))
+  )
+})
+
+//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
+self.addEventListener('activate', e => {
+  const cacheWhitelist = [CACHE_NAME]
+
+  e.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            //Eliminamos lo que ya no se necesita en cache
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName)
+            }
+          })
+        )
+      })
+      // Le indica al SW activar el cache actual
+      .then(() => self.clients.claim())
+  )
+})
+
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => {
+        if (res) {
+        
+          return res
+        }
+        return fetch(e.request)
+      })
+  )
+})
